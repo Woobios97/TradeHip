02327be82f53d1004a5006bd1e38a77f2d2f05b3
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-11-16 18:21:59 +0900

    feature/세부사항업로드

--- TradeHip/TradeHip/Managers/HapticsManager.swift
+++ TradeHip/TradeHip/Managers/HapticsManager.swift
@@ -15,16 +15,15 @@ final class HapticsManager {
     
     // MARK: - Public
     
-    /// <#Description#>
+    /// 선택 탭 상호작용을 위해 가볍게 진동을 실행
     public func vibrateForSelection() {
-        // 선택 탭 상호작용을 위해 가볍게 진동
         let generator = UISelectionFeedbackGenerator()
         generator.prepare()
         generator.selectionChanged()
     }
     
-    /// 주어진 유형의 상호작용에 대해 햅틱을 재생합니다.
-    /// - Parameter type: <#type description#>
+    /// 주어진 유형의 상호작용에 대해 햅틱을 재생
+    /// - Parameter type: 실행할 햅틱 피드백 유형
     public func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType) {
         let generator = UINotificationFeedbackGenerator()
         generator.prepare()
